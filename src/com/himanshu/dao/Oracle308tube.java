7bfd71f21f1ae7d5598bfc0b0e0a24e328b18daf
codered93 <himanshujsr@yahoo.co.in>
2017-12-18 18:19:17 +0530

    day-2 completed

---
+++ src/com/himanshu/dao/Oracle308tube.java
@@ -0,0 +1,24 @@
+package com.himanshu.dao;
+
+import javax.naming.*;
+import javax.sql.*;
+
+public class Oracle308tube {
+	private static DataSource Oracle308tube=null;
+	private static Context context=null;
+	public static DataSource Oracle308tubeConn()throws Exception{
+		if(Oracle308tube!=null)
+			return Oracle308tube;
+		try{
+			if(context==null)
+			{
+				context=new InitialContext();
+			}
+			Oracle308tube=(DataSource) context.lookup("308tubeOracle");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return Oracle308tube;
+	}
+}
--- src/com/himanshu/rest/status/V1_status.java
+++ src/com/himanshu/rest/status/V1_status.java
@@ -2,7 +2,9 @@ package com.himanshu.rest.status;
 
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.*;
+import java.sql.*;
+import com.himanshu.dao.*;
 
 @Path("/v1/status")
 public class V1_status {
@@ -21,5 +23,36 @@ public class V1_status {
 	public String returnVersion()
 	{
 		return "<p>Version: </p>"+api_version;
+	}
+	
+	@Path("/database")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnDatabaseStatus() throws Exception{
+		PreparedStatement query=null;
+		String myString=null;
+		String returnString=null;
+		Connection conn=null;
+		try{
+			conn= Oracle308tube.Oracle308tubeConn().getConnection();
+			query=conn.prepareStatement("select to_char(sysdate,'YYYY-MM-DD HH24:MI:SS') DATETIME"+
+					" from sys.dual");
+			/*System.out.println("Sample1 "+"select to_char(sysdate,'YYYY-MM-DD HH24:MI:SS') DATETIME"+
+					" from sys.dual");*/
+			ResultSet rs= query.executeQuery();
+			while(rs.next())
+			{
+				myString=rs.getString("DATETIME");
+			}
+			query.close();
+			returnString="<p>Database Status</p>"+"<p>Database Date/Time return: "+myString+"</p>";
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally{
+			if(conn!=null) conn.close();
+		}
+		return returnString;
 	}
 }
